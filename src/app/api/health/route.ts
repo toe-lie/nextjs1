2fe8d152dc258bf21b794d8c1fb92c3da6904ad2
“Toe <toelie.dev@gmail.com>
2025-06-01 19:52:42 +0700

    Cache Next.js build

--- src/app/api/health/route.ts
+++ src/app/api/health/route.ts
@@ -1,8 +1,6 @@
 // app/api/health/route.ts
-import { PrismaClient } from "@prisma/client";
+import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
-
-const prisma = new PrismaClient();
 
 export async function GET() {
   try {
